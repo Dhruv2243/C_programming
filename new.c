13958cc1062a49776583b3ba74b76e5a66f536d3
Dhruv Mittal <107043183+Dhruv2243@users.noreply.github.com>
2023-06-08 23:16:26 +0530

    Add files via upload

---
+++ new.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    int main()
+    {
+char a='A';
+char b='a';
+printf("the value of A = %d",a);
+printf("\n the ascii vale of a = %d",b);
+printf("\n the addition of two character %d+%d=%d",a,b,a+b);
+return 0;
+    }
---
+++ opa.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("ENTER THE VALUE");
+    scanf("%d",&a);
+    printf("ENTER THE SECOND VALUE");
+    scanf("%d",&b);
+    printf("\n THE ADDITION OF TWO NUMBER A+B=%d",a+b);
+    printf("\nTHE SUBTRACTION OF TWO NUMBER A-B=%d",a-b);
+    printf("\nTHE MULTIPLE OF TWO NUMBER A*B=%d",a*b);
+    printf("\nTHE DIVISION OF TWO NUMBER A/B=%d",a/b);
+    return 0;
+    }
